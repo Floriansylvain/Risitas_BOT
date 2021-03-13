9bed489ca9ce6346e5d68012be6db30cba90e55f
Florian SYLVAIN <floriansylvainpro@gmail.com>
2021-03-13 13:55:45 +0100

    📝 - Added a private.py sample

---
+++ python/private_sample.py
@@ -0,0 +1,9 @@
+TOKEN_BOT = '' # Discord bot token
+TOKEN_RIOT = '' # Riot app token
+TOKEN_TWITCH = '' # Twitch app token
+TOKEN_OSU = '' # Osu! app token
+ID_TWITCH = '' # Twitch app ID
+
+SERVER = '' # Twitch IRC server
+PORT = int() # Twitch IRC server port
+NICKNAME = '' # Twitch nickname
